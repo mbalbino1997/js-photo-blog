1876ef66ab9458157d49f36939c1ccfb365b85b1
mbalbino1997 <rmassimo.balbino@gmail.com>
2024-11-06 16:55:34 +0100

    add

---
+++ js/script.js
@@ -0,0 +1,11 @@
+/*
+<div class="col">
+    <div class="card">
+        <img class="pin" src="./img/pin.svg" alt="">
+        <figure>
+            <img class="testImg" src="./img/testimg.jfif" alt="">
+        </figure>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Non, optio.</p>
+    </div>
+</div>
+*/
