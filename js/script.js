28761e1fb45809dcc360fa5e9ab668a7e1810788
mbalbino1997 <rmassimo.balbino@gmail.com>
2024-11-06 18:11:43 +0100

    add

--- js/script.js
+++ js/script.js
@@ -9,3 +9,25 @@
     </div>
 </div>
 */
+const appendCard = document.getElementById("appendCard");
+let posts;
+axios.get("https://jsonplaceholder.typicode.com/photos?_limit=6")
+    .then(res => {
+        res.data.forEach(post => {
+            const { url, title } = post;
+            appendCard.innerHTML +=
+                `<div class="col">
+                    <div class="card">
+                        <img class="pin" src="./img/pin.svg" alt="">
+                        <figure>
+                            <img class="testImg" src="${url}" alt="">
+                        </figure>
+                         <p>${title}</p>
+                    </div>
+                </div>`
+
+        });
+    })
+    .catch(err => {
+        console.error(err);
+    })
