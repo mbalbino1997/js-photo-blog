62206f7905a0876289241c59863bd7fec39f66ad
mbalbino1997 <rmassimo.balbino@gmail.com>
2024-11-07 11:23:35 +0100

    add: overlay logic

--- js/script.js
+++ js/script.js
@@ -10,24 +10,43 @@
 </div>
 */
 const appendCard = document.getElementById("appendCard");
+const btnClose = document.getElementById("btnClose");
+const overlay = document.getElementById("overlay");
+const body = document.getElementById("body");
 let posts;
 axios.get("https://jsonplaceholder.typicode.com/photos?_limit=6")
     .then(res => {
-        res.data.forEach(post => {
-            const { url, title } = post;
+        res.data.forEach((post) => {
+            const { url, title, } = post;
             appendCard.innerHTML +=
                 `<div class="col">
-                    <div class="card">
+                    <div id="card" class="card">
                         <img class="pin" src="./img/pin.svg" alt="">
                         <figure>
                             <img class="testImg" src="${url}" alt="">
                         </figure>
                          <p>${title}</p>
                     </div>
-                </div>`
-            
+                </div>`;
+
+                const card = document.getElementById("card");
+                const img = document.getElementById("img");
+                card.addEventListener("click",() => {
+                    img.src=`"${url}"`;
+                    overlay.classList.remove("none");
+                    body.classList.add("overflow-hidden");
+                })
+
+
+
+
         });
     })
     .catch(err => {
         console.error(err);
+    })
+
+    btnClose.addEventListener("click",() => {
+        overlay.classList.add("none");
+        body.classList.remove("overflow-hidden");
     })
